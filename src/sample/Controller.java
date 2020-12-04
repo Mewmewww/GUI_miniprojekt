10fe43f68ef91d2cd243297fe5667986caa1ffa9
Drain <Deniz.yandim00@gmail.com>
2020-12-04 13:16:36 +0100

    new stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,27 +1,34 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.Expression;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.sql.Time;
-import java.util.Date;
+import java.io.*;
+import java.time.LocalTime;
+import java.util.Objects;
 import java.util.Random;
+import java.util.Scanner;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import java.util.concurrent.TimeUnit;
 
-import javax.management.timer.Timer;
+import javax.swing.*;
 
 
 public class Controller {
 
+    public Button textbutton;
+    public Button textbutton1;
     // koble fxml til java
+
     @FXML
     TextField textField1;
     public TextField valueA;
@@ -33,6 +40,30 @@ public class Controller {
 
     @FXML
     TextArea textArea;
+
+    @FXML
+    TextField textFieldReview;
+
+    @FXML
+    TextField textFieldReview1;
+
+    @FXML
+    TextArea textAreaStuff;
+
+    @FXML
+    TextArea textAreaStuff1;
+
+    @FXML
+    TextField insertttile;
+
+    @FXML
+    TextField insertttile1;
+
+    @FXML
+    TextArea textLotto;
+
+    @FXML
+    TextField textFieldLotto;
 
     int action = 1;
 
@@ -143,124 +174,189 @@ public class Controller {
         }
     }
 
-    public void date() {
-        Date date = new Date();
 
-    }
 
     public void dice() throws IOException, InterruptedException {
         imageView.setVisible(true);
         Random rand = new Random();
         int roll = rand.nextInt((6 - 1) + 1) + 1;
+
         textArea.setText("");
 
         textArea.appendText(String.valueOf(roll));
         textArea.setText(String.valueOf(roll));
-        switch (roll) {
+        new Thread(() -> {
 
-            case 1 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-1.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
+            switch (roll) {
 
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
+                case 1 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-1.png");
+                }
+
+                case 2 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-2.png");
+
+                }
+                case 3 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-3.png");
+
+
+                }
+                case 4 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-4.png");
+                }
+                case 5 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-5.png");
+
+                }
+                case 6 -> {
+                    tryCatch("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-6.png");
+                }
+                default -> {
+                }
             }
+                try {
+                    Thread.sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Platform.runLater(() -> {
+                    try {
+                        FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
+                        Image diceImgtwo = new Image(inputstreamtwo);
+                        imageView.setImage(diceImgtwo);
+                        inputstreamtwo.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
-            case 2 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-2.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
+                });
+        }).start();
 
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-
-            }
-            case 3 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-3.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
-
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-
-            }
-            case 4 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-4.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
-
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-            }
-            case 5 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-5.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
-
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-
-            }
-            case 6 -> {
-                FileInputStream inputstream = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/inverted-dice-6.png");
-                Image diceImg = new Image(inputstream);
-                imageView.setImage(diceImg);
-                inputstream.close();
-
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-
-
-            }
-            default -> {
-                Thread.sleep(1000);
-                FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-                Image diceImgtwo = new Image(inputstreamtwo);
-                imageView.setImage(diceImgtwo);
-                inputstreamtwo.close();
-            }
-
-        }
-        /*
-        FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-        Image diceImgtwo = new Image(inputstreamtwo);
-        imageView.setImage(diceImgtwo);
-        inputstreamtwo.close();
-        */
-        // Meddelse til Andras - i literally CANT get this to work the way i want to.
-        // Once I press "Roll the Dice" - I want it to show the dice that the value landed on (works) - but after
-        // two seconds (Wanted to use thread.sleep), the imageView will return to its original gif (where it constantly
-        // changes between dice 1-3) halp plz.
-
-        // Okay, so this is NOT WORKING
         action = 6;
     }
 
-    public void Spin() throws IOException, InterruptedException {
-        imageView.setVisible(true);
+    private void tryCatch(String path) {
+        try {
+            FileInputStream inputstreamtwo = new FileInputStream(path);
+            Image diceImgtwo = new Image(inputstreamtwo);
+            imageView.setImage(diceImgtwo);
+            inputstreamtwo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        FileInputStream inputstreamtwo = new FileInputStream("C:/Users/NoOne/IdeaProjects/GUI_miniprojekt/src/sample/dicegif.gif");
-        Image diceImgtwo = new Image(inputstreamtwo);
-        imageView.setImage(diceImgtwo);
-        inputstreamtwo.close();
+    public void logs() {
+        boolean result;
+
+        try {
+
+            File file = new File(insertttile.getText() + ".txt");
+            result = file.createNewFile();
+            if (result) {
+                textFieldReview.appendText("File created - shutting down" + file.getCanonicalPath());
+                FileWriter fileWriter = new FileWriter(file, true);
+                BufferedWriter outStream = new BufferedWriter(fileWriter);
+                outStream.append(textAreaStuff.getText());
+                outStream.close();
+                fileWriter.close();
+                System.exit(1);
+            }
+            else {
+                textFieldReview.appendText("File existing at " + file.getCanonicalPath());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
+    }
+    public void logsLommeregner() {
+        boolean result;
+
+        try {
+
+            File file = new File(insertttile1.getText() + ".txt");
+            result = file.createNewFile();
+            if (result) {
+                textFieldReview1.appendText("File created - shutting down" + file.getCanonicalPath());
+                FileWriter fileWriter = new FileWriter(file, true);
+                BufferedWriter outStream = new BufferedWriter(fileWriter);
+                outStream.append(textAreaStuff1.getText());
+                outStream.close();
+                fileWriter.close();
+                System.exit(1);
+            }
+            else {
+                textFieldReview1.appendText("File existing at " + file.getCanonicalPath());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+
+    }
+
+    public void Lotto() {
+
+        int[] numbers = new int[45];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int newnumber = random.nextInt((90 - 1) + 1) + 1;
+
+            for (int n = 0; n < numbers.length; n++) {
+
+                if (newnumber == numbers[n]) {
+
+                    while (newnumber == numbers[n]) {
+
+                        newnumber = random.nextInt((90 - 1) + 1) + 1;
+                        n = 0;
+
+                    }
+
+                }
+
+            }
+
+            numbers[i] = newnumber;
+            System.out.println(newnumber);
+
+        }
+        if (textLotto.getText() == null || textLotto.getText().trim().isEmpty()) {
+            errorMessage.setStyle("-fx-opacity: 1");
+            textLotto.appendText("This is empty - fill a value in it");
+
+        } else {
+        for (int i = 0; i < numbers.length; i++) {
+
+                Scanner scanner = new Scanner(textLotto.getText());
+
+                while (scanner.hasNextLine()) {
+                    String currentInt = scanner.nextLine();
+                    if (Integer.parseInt(currentInt) == numbers[i]) {
+
+                        textFieldLotto.clear();
+                        // textLotto.appendText(String.valueOf(currentNumber));
+                        textFieldLotto.appendText("It worked");
+                        return;
+                    } else {
+                        textFieldLotto.clear();
+                        // textLotto.appendText(String.valueOf(currentNumber));
+                        textFieldLotto.appendText("It didnt work");
+                    }
+
+                }
+
+            }
+        }
+
+
     }
 
 }
